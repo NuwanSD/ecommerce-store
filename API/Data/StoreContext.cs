eecb051150b903ee76e0484f3c391eaadb5c989d
NuwanSD <sagaradnuwan@gmail.com>
2025-03-23 11:58:08 +0530

    added identity user

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,11 +1,24 @@
 ﻿using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class StoreContext(DbContextOptions options) : DbContext(options)
+public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public required DbSet<Product> Products { get; set; }
 
     public required DbSet<Basket> Baskets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole>()
+            .HasData(
+                new IdentityRole { Id = "84253d2d-d0b0-4005-8e81-944a4814aa57", Name = "Member", NormalizedName = "MEMBER" },
+                new IdentityRole { Id = "db1cba9a-9d07-46f7-a1a0-61c03616a6b4", Name = "Admin", NormalizedName = "ADMIN" }
+            );
+    }
 }
